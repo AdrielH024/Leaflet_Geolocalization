8edcfb102ae818dcf62dac972f3bba4a6eebb43f
AdrielH024 <adriel.henrique024@gmail.com>
2023-08-08 10:25:52 -0300

    first alterations

--- server.js
+++ server.js
@@ -32,21 +32,31 @@ app.get('/', function (req, res) {
     res.sendFile("index.html" );
  })
  
+app.get("/api/city/:_city",async (req,res)=>{
+  const city = req.params._city;
+  try{
+    const resp = await art.find({cidade: city});
+    res.json(resp);
+    
+  }catch(err){
+    console.log(err);
+  }
+
+});
+
  app.post('/api',async (req,res)=>{
    const obj = req.body;
    console.log(obj.lati + " " + obj.logi);
    
-
-
    try{
-    var response = await fetch(`https://api.geoapify.com/v1/geocode/reverse?lat=${obj.logi}&lon=${obj.lati}&format=json&apiKey=7402c93da75c429f921df8320aca7b68`);
+    var response = await fetch(`https://api.geoapify.com/v1/geocode/reverse?lat=${obj.logi}&lon=${obj.lati}&format=json&apiKey=YOUR_key`);
     const data = await response.json();
     console.log(data.results[0].city);
     let pic = new art({
       name:obj.name,
       author_id:obj.authorname,
-      longi:obj.logi,
-      lati: obj.lati,
+      longi:obj.lati,
+      lati: obj.logi,
       cidade: data.results[0].city
      });
     await pic.save();
@@ -58,7 +68,7 @@ app.get('/', function (req, res) {
 
 var server = app.listen(8081, function () {
     var host = server.address().address
-    var port = server.address().port
+    var port = server.address().port 
     
     console.log("Example app listening at http://%s:%s", host, port)
  })
